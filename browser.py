49c89c617b29d42c87e17d3ed4a1bf22de6ee127
Austin Jackson <themacphage@gmail.com>
2019-01-04 01:05:16 +0000

    selenium start

--- browser.py
+++ browser.py
@@ -1,12 +1,24 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-import mechanize
-br = mechanize.Browser()
-#br.set_all_readonly(False)    # allow everything to be written to
-br.set_handle_robots(False)   # ignore robots
-#br.set_handle_refresh(False)  # can sometimes hang without this
-br.addheaders = [('User-agent', 'Firefox')]
+import os
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 
-response = br.open("https://howdy.tamu.edu")
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.binary_location = "/usr/bin/chromium"
 
-print( response.read())
+driver = webdriver.Chrome(executable_path=os.path.abspath("/usr/bin/chromedriver"),   chrome_options=chrome_options)
+driver.set_page_load_timeout(60) # 60 seconds
+time.sleep(1)
+
+driver.get("http://example.com")
+
+print(driver.current_url)
+
+title = driver.find_element_by_css_selector("h1")
+print(title.text)
+
+driver.close()
