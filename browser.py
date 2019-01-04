d8f4e8310bf6e2467c46ff8bfab92e46d7590ad2
Austin Jackson <themacphage@gmail.com>
2019-01-04 01:23:59 +0000

    test

--- browser.py
+++ browser.py
@@ -14,11 +14,11 @@ driver = webdriver.Chrome(executable_path=os.path.abspath("/usr/bin/chromedriver
 driver.set_page_load_timeout(60) # 60 seconds
 time.sleep(1)
 
-driver.get("http://example.com")
+driver.get("http://howdy.tamu.edu")
 
 print(driver.current_url)
 
-title = driver.find_element_by_css_selector("h1")
+title = driver.find_element_by_css_selector("#splash-cell-inner > p:nth-child(3) > i")
 print(title.text)
 
 driver.close()
